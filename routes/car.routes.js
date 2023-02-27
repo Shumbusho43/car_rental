bce3568156877a4f5538ee5ac5213ac7fb0124d7
Shumbusho43 <davidshumbusho10@gmail.com>
2023-02-27 20:05:53 +0200

    removed protect from get cars

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -12,12 +12,11 @@ const {
     role
 } = require('../utils/protect');
 const router = express.Router();
-router.use(protect);
 router.get("/get-borrowed-cars", protect, role('owner', 'admin'), getBorrowedCars);
-router.get("/get-free-cars", protect,getFreeCars);
+router.get("/get-free-cars", getFreeCars);
 router.route("/:id")
     .post(uploadCar, role('owner'))
     .get(getCarById)
-    .put(uploadCarImage, role('owner'))
+    .put(uploadCarImage,protect, role('owner'))
 router.get("/", getAllCars)
 module.exports.carRouter = router;
